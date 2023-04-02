a56e4c8dafb648752c5f621ed664457b92ac9a50
Julia Koblitz <julia.helmecke@tu-bs.de>
2023-04-02 19:16:41 +0200

    improved about

--- pages/news.php
+++ pages/news.php
@@ -1,35 +1,32 @@
-<div class="row">
-    <div class="col-lg-9">
 
-
-        <div class='content'>
+        <div class='container'>
             <h1>OSIRIS</h1>
-            <h4 class="text-muted font-weight-normal mt-0">The Open, Simple and Integrated Research Information System</h4>
+            <h4 class="text-osiris font-weight-normal mt-0">The Open, Simple and Integrated Research Information System</h4>
 
             <p>
                 <?= lang(
-                    'OSIRIS is my attempt to create a light-weight and open-source research information system; made-to-measure the needs of the DSMZ members. Thereby, usability is my key concern.',
-                    'OSIRIS ist mein Ansatz, ein leichtgewichtiges Open-Source Forschungsinformationssystem zu schaffen; maßgeschneidert auf die Bedürfnisse der DSMZ. Dabei ist die Nutzerfreundlichkeit der Seite mein größtes Anliegen.'
+                    'OSIRIS is my attempt to create a light-weight and open-source research information system; made-to-measure the needs of smaller institutes such as the DSMZ. Thereby, usability is my key concern.',
+                    'OSIRIS ist mein Ansatz, ein leichtgewichtiges Open-Source Forschungsinformationssystem zu schaffen; maßgeschneidert auf die Bedürfnisse kleinerer Institute wie der DSMZ. Dabei ist die Nutzerfreundlichkeit der Seite mein größtes Anliegen.'
                 ) ?>
             </p>
 
-            <blockquote class="mb-20">
-        <p>
-            A user interface is like a joke. If you have to explain it, it’s not that good”.
-        </p>
-        <em>
-            — Martin Leblanc</em>
-    </blockquote>
+            <blockquote class="mb-20 alert font-size-16" style="border-left: 5px solid var(--primary-color);">
+                <p>
+                    A user interface is like a joke. If you have to explain it, it’s not that good”.
+                </p>
+                <em>
+                    — Martin Leblanc</em>
+            </blockquote>
 
-            <hr>
+            <!-- <hr> -->
             <?php
-            include BASEPATH . "/pages/roadmap.php";
+            // include BASEPATH . "/pages/roadmap.php";
             ?>
-            <hr>
+            <!-- <hr> -->
             <?php
-            $text = file_get_contents(BASEPATH . "/todo.md");
-            $parsedown = new Parsedown;
-            echo $parsedown->text($text);
+            // $text = file_get_contents(BASEPATH . "/todo.md");
+            // $parsedown = new Parsedown;
+            // echo $parsedown->text($text);
             ?>
             <hr>
             <?php
@@ -37,7 +34,7 @@
             $parsedown = new Parsedown;
             echo $parsedown->text($text);
             ?>
-        </div>
+        <!-- </div>
     </div>
     <div class="col-lg-3 d-none d-lg-block">
         <nav class="on-this-page-nav">
@@ -46,8 +43,7 @@
                 <a href="#roadmap">Roadmap</a>
                 <a href="#to-do-liste">To-Do-Liste</a>
                 <a href="#changelog">Changelog</a>
-                <!-- <a href="#fragen-at-controlling">Fragen @ Controlling</a> -->
             </div>
         </nav>
-    </div>
+    </div> -->
 </div>
