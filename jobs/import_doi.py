3af3ff8ca5ac1dd97ee31566d6f5b9560bb53145
Julia Koblitz <julia.koblitz@dsmz.de>
2024-07-26 06:22:50 +0200

    Added error handling for the DOI import skript

--- jobs/import_doi.py
+++ jobs/import_doi.py
@@ -4,4 +4,13 @@ parser = OpenAlexParser()
 with open('jobs/doi.csv') as f:
     for line in f:
         line = line.strip()
-        parser.get_work(line, ignoreDupl=True)
+        try:
+            parser.get_work(line, ignoreDupl=True)
+        except Exception as e:
+            print(f'''
+            There was an error with the DOI: {line}
+            \t{e}
+            The DOI will be skipped.
+            ''')
+            print(line)
+            continue
